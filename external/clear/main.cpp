61d3c945ad2324f584127aee62cdb5a052151315
rogerxaic <roger.miret-gine@insa-lyon.fr>
2015-05-24 19:05:08 +0200

    Added clear
    Added clear.exe and .cpp source for Windows

---
+++ external/clear/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+    system("cls");
+    return 0;
+}
--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -1,4 +1,4 @@
-    /*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -19,14 +19,33 @@ public class Battleship {
      */
     public static void main(String[] args) throws IOException {
 
-        String OS = System.getProperty("os.name").toLowerCase();
-        String neteja = (OS.indexOf("win") >= 0) ? "ipconfig" : "clear";
         String username = System.getProperty("user.name");
+        clear();
+        System.out.println(username);
 
+        //System.out.println(neteja);
+    }
+
+    public static void clear() throws IOException {
+        String OS = System.getProperty("os.name").toLowerCase();
+        String neteja = (OS.indexOf("win") >= 0) ? "clear" : "clear";
+
+//        J'ai créé le fichier clear.exe (trouvable dans 
+//                                        ./external/clear/bin/Debug)
+//        J'ai placé ce fichier dans System32 pour que Windows ne 
+//        donne pas d'erreurs. La commande clear est cls, mais NetBeans ne veut
+//        pas la prendre car c'est cmd qui interprète cls, donc n'est pas un 
+//        executable Windows que NetBeans puisse executer.
         Process p = Runtime.getRuntime().exec(neteja);
-        
-        System.out.println(neteja);
 
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(p.getInputStream()))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
     }
 
 }
